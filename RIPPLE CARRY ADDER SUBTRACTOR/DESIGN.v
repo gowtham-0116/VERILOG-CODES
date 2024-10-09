0cbf7f5f0530c9402802e42a39de59e415ead4af
gowtham-0116 <gowthamkomagan0116@gmail.com>
2024-10-09 13:42:28 +0530

    Create DESIGN.v

---
+++ RIPPLE CARRY ADDER SUBTRACTOR/DESIGN.v
@@ -0,0 +1,21 @@
+module full_adder(
+  input a, b, cin,
+  output sum, cout
+);
+  assign {sum, cout} = {a^b^cin, ((a & b) | (b & cin) | (a & cin))};
+endmodule
+module ripple_carry_adder_subtractor #(parameter SIZE = 4) (
+  input [SIZE-1:0] A, B, 
+  input CTRL,
+  output [SIZE-1:0] S, Cout);
+  bit [SIZE-1:0] Bc;
+  genvar g;
+  assign Bc[0] = B[0] ^ CTRL;
+    full_adder fa0(A[0], Bc[0], CTRL, S[0], Cout[0]);
+  generate
+    for(g = 1; g<SIZE; g++) begin
+      assign Bc[g] = B[g] ^ CTRL;
+      full_adder fa(A[g], Bc[g], Cout[g-1], S[g], Cout[g]);
+    end
+  endgenerate
+endmodule
