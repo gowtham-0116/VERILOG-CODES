408c94ab46e37ebe243c00dc424bece3340e6989
gowtham-0116 <gowthamkomagan0116@gmail.com>
2024-10-22 09:46:08 +0530

    Create LOGICAL OPERATOR.v

---
+++ VERILOG OPERATORS/LOGICAL OPERATOR.v
@@ -0,0 +1,9 @@
+module logical_op;
+  reg[3:0] i1,i2;
+  initial begin
+    i1 = 4'h6; i2 =4'h2;
+    $display("For operator: (&&): i1 = %0h && i2 = %0h: %h", i1, i2, i1 && i2);
+    $display("For operator: (||): i1 = %0h || i2 = %0h: %h", i1, i2, i1 || i2);
+    $display("For operator: (!) : i1 = %0h !  i2 = %0h: %h", i1, i2, !i1);
+  end 
+endmodule
