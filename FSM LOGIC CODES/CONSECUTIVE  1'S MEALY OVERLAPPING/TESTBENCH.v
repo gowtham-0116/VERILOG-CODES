06edd5964d379e8a11536d352c159a694341e953
gowtham-0116 <gowthamkomagan0116@gmail.com>
2024-11-06 15:18:03 +0530

    Update TESTBENCH.v

--- FSM LOGIC CODES/CONSECUTIVE  1'S MEALY OVERLAPPING/TESTBENCH.v
+++ FSM LOGIC CODES/CONSECUTIVE  1'S MEALY OVERLAPPING/TESTBENCH.v
@@ -4,20 +4,29 @@ module tb;
   reg c;
   wire d;
   
-  consecutive_1s_mealy_overlapping c1mo(clk,reset,c,d);
+  three_consecutive_1s_mealy_overlapping c1mo(clk,reset,c,d);
   initial begin
     clk=0;
-  forever #5; clk=~clk;
+  forever #5 clk= ~clk;
   end
   
   initial begin
     reset=1;
-    c=0; #5;
+    c=0; #50;
     reset=0;
     c=1; #5;
     c=1; #5;
     c=1; #5;
+    c=0; #5;
+    c=0; #5;
+    c=0; #5;
+    c=1; #5;
+    c=1; #5;
+    c=1; #5;
     $finish;
+  end
+  initial begin
+    $monitor("clk=%0h,reset=%0h,c=%0h,d=%0h",$time , clk,reset,c,d);
   end
    initial begin
     $dumpfile("waves.vcd");
