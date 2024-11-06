39bfb967706100375ac88de15e7971b2c23837fb
gowtham-0116 <gowthamkomagan0116@gmail.com>
2024-11-06 15:28:40 +0530

    Update TESTBENCH.v

--- FSM LOGIC CODES/SEQUENCE DETECT MORE THAN ONE 1'S/TESTBENCH.v
+++ FSM LOGIC CODES/SEQUENCE DETECT MORE THAN ONE 1'S/TESTBENCH.v
@@ -7,17 +7,20 @@ module tb;
   seq_detect_more_than_one_1s sdmto1(clk,reset,c,d);
   initial begin
     clk=0;
-  forever #5; clk=~clk;
+  forever #10 clk=~clk;
   end
   
   initial begin
     reset=1;
-    c=0; #5;
+    c=0; #30;
     reset=0;
-    c=1; #5;
-    c=1; #5;
-    c=0; #5;
+    c=1; #10;
+    c=1; #10;
+    c=0; #10;
     $finish;
+  end
+  initial begin
+    $monitor("clk=%0h,reset=%0h,c=%0h,d=%0h",$time , clk,reset,c,d);
   end
    initial begin
     $dumpfile("waves.vcd");
