54efd0b82985d56634670fd396462e898563a12f
gowtham-0116 <gowthamkomagan0116@gmail.com>
2024-11-06 16:15:21 +0530

    Create DESIGN.v

---
+++ FSM LOGIC CODES/60% CLOCK DUTY CYCLE/DESIGN.v
@@ -0,0 +1,19 @@
+module duty_cycle(input in_clk, rst, output reg out_clk);
+  reg [3:0] counter;
+  
+  always@(posedge in_clk or posedge rst) begin
+    if(rst) begin
+      counter <= 0;
+      out_clk <= 0;
+    end
+    else begin
+        counter <= counter+1;
+      if(counter<6)
+        out_clk <= 1;
+      else
+        out_clk<=0;
+      if(counter==9)
+        counter<=0;
+    end
+  end
+endmodule
