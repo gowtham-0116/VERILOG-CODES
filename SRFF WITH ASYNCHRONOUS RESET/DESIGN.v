ef2d6b75ebda9c680415db5bbe244a249aff2dcd
gowtham-0116 <gowthamkomagan0116@gmail.com>
2024-11-08 15:30:02 +0530

    Create DESIGN.v

---
+++ SRFF WITH ASYNCHRONOUS RESET/DESIGN.v
@@ -0,0 +1,20 @@
+module sr_ff_async(
+  input clk,
+  input S,R,
+  input reset,
+  output reg Q,
+  output reg Q_bar);
+  always@(posedge clk or negedge reset ) begin
+    if(reset)
+      Q<=0;
+    else begin
+    case({S,R})
+      2'b00: Q <= Q;
+      2'b01: Q <= 1'b0;
+      2'b10: Q <= 1'b1;
+      2'b11: Q <= 1'bx;
+    endcase
+    end
+  end
+  assign Q_bar = ~Q;
+endmodule
